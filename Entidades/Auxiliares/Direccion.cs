ff9f753eb58f40220a82221437bec99571627088
dpy314 <d.py@ant-informatica.com>
2024-07-31 16:51:36 +0200

    Mantenimiento clientes

---
+++ Entidades/Auxiliares/Direccion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANTEntidades.Entidades.Auxiliares
+{
+    public class Direccion
+    {
+        public string TipoVia { get; set; }
+        public string Calle { get; set; }
+        public string Numero { get; set; }
+        public string CP { get; set; }
+        public string Poblacion { get; set; }
+        public string Provincia { get; set; }
+        public string Pais { get; set; }
+    }
+}
--- Entidades/Maestros/Cliente.cs
+++ Entidades/Maestros/Cliente.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ANTEntidades.Entidades.Auxiliares;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,30 +11,37 @@ namespace ANTEntidades.Maestros
     public class Cliente
     {
         #region Propiedades
-
+        //****************************
+        //          GENERAL
+        //****************************
         // Cabecera
         public string IdCliente { get; set; }
-        public string IdEmpresa { get; set; }
         public string Nombre { get; set; }
+        public string RazonSocial { get; set; }
         public string Mote { get; set; }
         public string NIF { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public bool Activo { get; set; }
 
-        // Direccion Fiscal
-        public string TipoVia { get; set; }
-        public string Direccion { get; set; }
-        public string NumDireccion { get; set; }
-        public string CP { get; set; }
-        public string Poblacion { get; set; }
-        public string Provincia { get; set; }
-        public string Pais { get; set; }
+        // Direcciones
+        public Direccion DireccionFiscal { get; set; }
+        public Direccion DireccionComercial { get; set; }
+        public Direccion DireccionEnvio { get; set; }
+
+        //****************************
+        //          FACTURACION
+        //****************************
+        public string FormaPago { get; set; }
+        public string DocPago { get; set; }
+        public string Moneda { get; set; }
+        public string Iban { get; set; }
+        public int PlazoMaxPago { get; set; } // Dias que puede estar una factura sin pagar
+        public string FormaFacturacion { get; set; } // Si se le pueden hacer (F) Facturas, (A) Albaranes o (T) ambos a documentos nuevos
 
         // Descuentos y Pagos
         public string RegimenIva { get; set; }
         public double Iva { get; set; }
         public string TipoIva { get; set; }
-        public string FormaPago { get; set; }
-        public string DocPago { get; set; }
-        public string Moneda { get; set; }
 
         // Contactos
         public List<Contacto> Contactos { get; set; }
